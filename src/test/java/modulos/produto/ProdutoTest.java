54c1d6f6fa207b2dd30d2ea7d75878a194690c53
Cristiane Abreu <abreu.kr@gmail.com>
2022-03-07 20:30:08 +0000

    Initial commit

---
+++ src/test/java/modulos/produto/ProdutoTest.java
@@ -0,0 +1,100 @@
+package modulos.produto;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.concurrent.TimeUnit;
+
+@DisplayName("Testes Mobile do Módulo de Produtos")
+public class ProdutoTest {
+    @DisplayName("Validação do Valor de Produto Não Permitido")
+    @Test
+    public void testValidacaoDoValorDeProdutoNaoPermitido() throws MalformedURLException {
+        // Abrir o APP
+        DesiredCapabilities capacidades = new DesiredCapabilities();
+        capacidades.setCapability("deviceName","Google Pixel 3");
+        capacidades.setCapability("platform","Android");
+        capacidades.setCapability("udid", "192.168.88.102:5555");
+        capacidades.setCapability("appPackage", "com.lojinha");
+        capacidades.setCapability("appActivity", "com.lojinha.ui.MainActivity");
+        capacidades.setCapability("app", "C:\\Users\\crist\\Documents\\Arquivos de Suporte do Módulo 11\\Lojinha+Android+Nativa\\lojinha-nativa.apk");
+
+        WebDriver app = new RemoteWebDriver(new URL("http://127.0.0.1:4723/wd/hub"), capacidades);
+        app.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+
+        // Fazer Login
+        app.findElement(By.id("com.lojinha:id/user")).click();
+        app.findElement(By.id("com.lojinha:id/user")).findElement(By.id("com.lojinha:id/editText")).sendKeys("admin");
+        app.findElement(By.id("com.lojinha:id/password")).click();
+        app.findElement(By.id("com.lojinha:id/password")).findElement(By.id("com.lojinha:id/editText")).sendKeys("admin");
+
+        app.findElement(By.id("com.lojinha:id/loginButton")).click();
+
+        // Abrir formulário de novo produto
+        app.findElement(By.id("com.lojinha:id/floatingActionButton")).click();
+
+        // Cadastrar um produto com valor inválido
+        app.findElement(By.id("com.lojinha:id/productName")).click();
+        app.findElement(By.id("com.lojinha:id/productName")).findElement(By.id("com.lojinha:id/editText")).sendKeys("Playstation 64");
+        app.findElement(By.id("com.lojinha:id/productValue")).click();
+        app.findElement(By.id("com.lojinha:id/productValue")).findElement(By.id("com.lojinha:id/editText")).sendKeys("700001");
+        app.findElement(By.id("com.lojinha:id/productColors")).click();
+        app.findElement(By.id("com.lojinha:id/productColors")).findElement(By.id("com.lojinha:id/editText")).sendKeys("Branco, Verde, Azul, Cinza");
+
+        app.findElement(By.id("com.lojinha:id/saveButton")).click();
+
+        // Validar que a mensagem de valor inválido foi apresentada
+        String mensagemApresentada = app.findElement(By.xpath("//android.widget.Toast")).getText();
+        Assertions.assertEquals("O valor do produto deve estar entre R$ 0,01 e R$ 7.000,00", mensagemApresentada);
+
+    }
+
+    @DisplayName("Validação de Produto Cadastrado com Sucesso")
+    @Test
+    public void testValidacaoDeProdutoCadastradoComSucesso() throws MalformedURLException {
+        // Abrir o APP
+        DesiredCapabilities capacidades = new DesiredCapabilities();
+        capacidades.setCapability("deviceName","Google Pixel 3");
+        capacidades.setCapability("platform","Android");
+        capacidades.setCapability("udid", "192.168.88.102:5555");
+        capacidades.setCapability("appPackage", "com.lojinha");
+        capacidades.setCapability("appActivity", "com.lojinha.ui.MainActivity");
+        capacidades.setCapability("app", "C:\\Users\\crist\\Documents\\Arquivos de Suporte do Módulo 11\\Lojinha+Android+Nativa\\lojinha-nativa.apk");
+
+        WebDriver app = new RemoteWebDriver(new URL("http://127.0.0.1:4723/wd/hub"), capacidades);
+        app.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+
+        // Fazer Login
+        app.findElement(By.id("com.lojinha:id/user")).click();
+        app.findElement(By.id("com.lojinha:id/user")).findElement(By.id("com.lojinha:id/editText")).sendKeys("admin");
+        app.findElement(By.id("com.lojinha:id/password")).click();
+        app.findElement(By.id("com.lojinha:id/password")).findElement(By.id("com.lojinha:id/editText")).sendKeys("admin");
+
+        app.findElement(By.id("com.lojinha:id/loginButton")).click();
+
+        // Abrir formulário de novo produto
+        app.findElement(By.id("com.lojinha:id/floatingActionButton")).click();
+
+        // Cadastrar um produto com valor válido
+        app.findElement(By.id("com.lojinha:id/productName")).click();
+        app.findElement(By.id("com.lojinha:id/productName")).findElement(By.id("com.lojinha:id/editText")).sendKeys("Playstation 64");
+        app.findElement(By.id("com.lojinha:id/productValue")).click();
+        app.findElement(By.id("com.lojinha:id/productValue")).findElement(By.id("com.lojinha:id/editText")).sendKeys("99900");
+        app.findElement(By.id("com.lojinha:id/productColors")).click();
+        app.findElement(By.id("com.lojinha:id/productColors")).findElement(By.id("com.lojinha:id/editText")).sendKeys("Branco, Verde");
+
+        app.findElement(By.id("com.lojinha:id/saveButton")).click();
+
+        // Validar que a mensagem de sucesso foi apresentada
+        String mensagemApresentada = app.findElement(By.xpath("//android.widget.Toast")).getText();
+        Assertions.assertEquals("Produto adicionado com sucesso", mensagemApresentada);
+
+    }
+}
